b7002fbd730f3bcb28c665b278b62109d3a7b55b
oraxswitcher <raouf200026@gmail.com>
2020-11-15 01:18:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./imgs/darel3ayla1.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Satisfy&display=swap" rel="stylesheet">   
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Dar El 3ayla | Welcome</title>
+</head>
+<body>
+    <header>
+        <div class="nav">
+            <a href="#"><img class="logo" src="./imgs/darel3ayla1.png" alt="darel3ayla"></a>
+            <div class="navbar">
+                <a href="#">Home</a>
+                <a href="#">Our location</a>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-whatsapp"></i></a>
+            </div>
+        </div>
+        <div class="cont">
+            <div class="bio">
+            <h3>FOR GURGLING BELLIES</h3>
+               <h3>SWEET AND SALTY</h3>
+            </div>
+        </div>
+    </header>
+    <section class="section1">
+        <div class="tutvid">
+            <h1>Dar El 3ayla</h1>
+            <video class="vid" muted controls loop autoplay>
+                <source src="./imgs/darel3aylavid.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="box box1">
+            <h2>Welcome to our hamble coffee shop Here you are going to rest and enjoy the setting with your friends or having a complete peace with your self </h2>
+            <img src="./imgs/16.jpg" alt="16">
+        </div>
+        <div class="box box2">
+            <img src="./imgs/1.jpg" alt="1">
+            <h2>In our shop we have so many types of food that will make you happy by only a single bite </h2>
+        </div>
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+        
+                <div class="swiper-slide"><img src="./imgs/17.jpg" alt="17"></div>
+                <div class="swiper-slide"><img src="./imgs/2.jpg" alt="2"></div>
+                <div class="swiper-slide"><img src="./imgs/3.jpg" alt="3"></div>
+                <div class="swiper-slide"><img src="./imgs/19.jpg" alt="19"></div>
+                <div class="swiper-slide"><img src="./imgs/5.jpg" alt="5"></div>
+                <div class="swiper-slide"><img src="./imgs/6.jpg" alt="6"></div>
+                <div class="swiper-slide"><img src="./imgs/7.jpg" alt="7"></div>
+                <div class="swiper-slide"><img src="./imgs/9.jpg" alt="9"></div>
+                <div class="swiper-slide"><img src="./imgs/10.jpg" alt="10"></div>
+                <div class="swiper-slide"><img src="./imgs/13.jpg" alt="13"></div>
+                <div class="swiper-slide"><img src="./imgs/14.jpg" alt="14"></div>
+                <div class="swiper-slide"><img src="./imgs/18.jpg" alt="18"></div>
+                <div class="swiper-slide"><img src="./imgs/21.jpg" alt="21"></div>
+                <div class="swiper-slide"><img src="./imgs/22.jpg" alt="22"></div>
+                <div class="swiper-slide"><img src="./imgs/23.jpg" alt="23"></div>
+                <div class="swiper-slide"><img src="./imgs/24.jpg" alt="24"></div>
+                
+        
+            </div>
+    
+            <div class="swiper-pagination"></div>
+
+    
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+
+    
+            <div class="swiper-scrollbar"></div>
+        </div>
+    </section>
+    <footer>
+        <div class="support">
+            <a href="#"><img src="./imgs/darel3ayla1.png" alt="logo"></a>
+        </div>
+        
+        <div class="contact">
+            <h3>Contact-Us</h3>
+            <div class="social">
+                <a href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+                <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+                <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+                <a href="#"><i class="fab fa-whatsapp"></i> Whatsapp</a>
+            </div>
+        </div>
+        <div class="informations">
+            <h3>Informations</h3>
+            <div class="informs">
+                <a href="#"><i class="fas fa-map-marker-alt"></i>Algeria, Jijel</a>
+                <a href="#"><i class="fas fa-phone-alt"></i>+213555555555</a>
+                <a href="#"><i class="far fa-envelope"></i>darel3ayla@gmail.com</a>
+            </div>
+        </div>
+    </footer>
+    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/fdddec0283.js" crossorigin="anonymous"></script>
+    <script src="./js/app.js"></script>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,20 @@
+var mySwiper = new Swiper('.swiper-container', {
+
+    effect: 'cube',
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
